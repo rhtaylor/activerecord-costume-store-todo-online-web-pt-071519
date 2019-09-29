b5efa34d6f1e364b8f2ddbddaead6a87fc4a4556
Randy Taylor <randyisbusy@gmail.com>
2019-09-29 20:27:22 +0000

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,17 @@
 # Create your costume_stores migration here
+class CreateCostumeStores < ActiveRecord::Migration[5.2]
+  def change
+    create_table :costume_stores do |t|
+      t.string :name
+      t.string :location
+      t.integer :costume_inventory
+      t.integer :num_of_employees
+      t.boolean :still_in_business
+      t.timestamp :opening_time
+      t.timestamp :closing_time
+      t.string :image_url
+      t.string :size
+      t.timestamps
+    end
+  end
+end
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,17 @@
 # Create your haunted_houses migration here
+class CreateHauntedHouses < ActiveRecord::Migration[5.2]
+  def change
+    create_table :haunted_houses do |t|
+      t.string :name
+      t.string :theme
+      t.string :location
+      t.float :price
+      t.boolean :family_friendly
+      t.timestamp :opening_date
+      t.timestamp :closing_date
+      t.string :description
+      t.string :size
+
+    end
+  end
+end
